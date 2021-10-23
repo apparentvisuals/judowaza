ca00325a8c2d4a62b417f2f6f9c8527f6654a5c5
apparentvisuals <3787826+apparentvisuals@users.noreply.github.com>
2021-10-22 22:28:28 -0400

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,234 @@
+<template>
+  <div role="hidden" class="mx-auto max-w-[9.6in] p-2 text-gray-800">
+    <main class="border-gray-200 p-4">
+      <header class="flex">
+        <logo class="w-12 h-12 border-gray-800" />
+        <h1 class="pl-4">Bailey Hu</h1>
+      </header>
+      <section>
+        <h2>summary</h2>
+        <p>
+          Experienced software developer with decade long experience managing,
+          designing and developing desktop, web and mobile applications in Java,
+          Objective-C, Swift and JavaScript. With certification in usability and
+          scrum, have successfully delivered many projects from start to finish.
+        </p>
+      </section>
+      <section>
+        <h2>experience</h2>
+        <h3>Blackberry</h3>
+        <article>
+          <h4 class="pl-2">Principal Systems Developer (2018 - Present)</h4>
+          <div role="none" class="grid grid-flow-col">
+            <aside class="summary">
+              <ul>
+                <li>Node.js</li>
+                <li>Rust</li>
+                <li>VueJS</li>
+                <li>Azure</li>
+                <li>Docker</li>
+                <li>Kubernetes</li>
+                <li>OAuth/SAML</li>
+              </ul>
+            </aside>
+            <div class="p-2"></div>
+          </div>
+        </article>
+        <h3>Buildmetric</h3>
+        <article>
+          <h4 class="pl-2">Co-founder / CTO (2015-2018)</h4>
+          <div role="none" class="grid grid-flow-col">
+            <aside class="summary">
+              <ul>
+                <li>Product owner</li>
+                <li>Azure</li>
+                <li>Node.js</li>
+                <li>MongoDB</li>
+                <li>Native iOS</li>
+                <li>Native Android</li>
+              </ul>
+            </aside>
+            <div class="p-2">
+              <p>
+                Graduate of Grindspace XL and the L-Spark incubator program.
+                Established a successful SaaS product serving the residential
+                construction market, leveraging Business Model Canvas and Design
+                Thinking methodologies to build customer personas and develop an
+                empathic understanding of their needs.
+              </p>
+              <p>
+                Responsible for the design and development of micro-service
+                based authentication and API services, rich client apps for web,
+                iOS and Android that increased customer satisfaction, produced
+                real-time analytics on quality and warranty claims.
+              </p>
+            </div>
+          </div>
+        </article>
+        <h3>Appconic</h3>
+        <article>
+          <h4 class="pl-2">Co-founder (2014 - 2018)</h4>
+          <div role="none" class="grid grid-flow-col">
+            <aside class="summary">
+              <ul>
+                <li>Product owner</li>
+                <li>Node.js</li>
+                <li>Ruby</li>
+                <li>Native iOS</li>
+                <li>Native Android</li>
+                <li>React Native</li>
+                <li>AWS</li>
+                <li>HIPPA/PIPEDA</li>
+              </ul>
+            </aside>
+            <div class="p-2">
+              <p>
+                Started a digital consulting agency with two other co-founders
+                focusing on web and mobile development, delivering more than 7
+                successful products in the construction and pharmaceutical space
+                that are still in use today. One of the products is used by
+                thousands of patients to manage their dosage and symptoms.
+              </p>
+              <p>
+                Responsible for requirement gathering and architecture design,
+                translating customer needs into technical requirements. Work
+                included writing architecture and design documentation for
+                mobile-only and client-server applications, translating the
+                design to the product backlog and managing delivery deadlines.
+              </p>
+            </div>
+          </div>
+        </article>
+        <h3>Blackberry</h3>
+        <article>
+          <h4 class="pl-2">
+            Senior Software Developer - Blackberry Messenger (2013-2014)
+          </h4>
+          <div role="none" class="grid grid-flow-col">
+            <aside class="summary">
+              <ul>
+                <li>Scrum master</li>
+                <li>Java</li>
+                <li>Native Android</li>
+                <li>Gradle</li>
+              </ul>
+            </aside>
+            <div class="p-2">
+              <p>
+                As part of the company transition to focus more on Android
+                devices I moved to the Blackberry Messenger team to focus on
+                mobile development. I was one of three developers on the team
+                growing to a size of 12 prior to the release of Blackberry
+                messenger for Android. Because of my prior experience I was
+                acting as the development lead and scrum master for the team.
+              </p>
+              <p>
+                I facilitated a cross team initiative between Android, iOS and
+                Blackberry groups that ensured UI/UX was consistent between all
+                platforms. Planned lunch and learn sessions for junior
+                developers on Android best practices and new technologies. Lead
+                daily stand-up with both developers and external contractors
+                maintaining high sprint velocity. Blackberry Messenger for
+                Android was successfully released in 2013.
+              </p>
+              <p></p>
+            </div>
+          </div>
+        </article>
+        <article>
+          <h4 class="pl-2">
+            Team Lead - Blackberry Link / Media Sync (2010-2013)
+          </h4>
+          <div role="none" class="grid grid-flow-col">
+            <aside class="summary">
+              <ul>
+                <li>Scrum master</li>
+                <li>C/C++/C#</li>
+                <li>Native Windows</li>
+                <li>NGINX</li>
+                <li>WebDAV</li>
+              </ul>
+            </aside>
+            <div class="p-2">
+              <p>
+                I was promoted to lead a team of 4 developers, responsible for
+                the core synchronization technology in Wi-Fi Media Sync /
+                BlackBerry Link. The solution contained a mobile client and a
+                desktop server. A custom WebDAV module was developed for NGINX
+                in C to deliver wireless synchronization capabilities to
+                BlackBerry devices along with a device core written in C++. I
+                was the primary architect for the protocol and received two
+                patents for wireless metadata synchronization and fast heuristic
+                generation of video thumbnails.
+              </p>
+              <p>
+                I transitioned the team from waterfall to scrum methodology,
+                adopting daily standup, sprint planning and requirement
+                management via a product backlog. Acting as the scrum master, I
+                worked closely with product managers across desktop and device
+                teams to remove blockers and ensure high sprint velocity. I was
+                also responsible for the hiring process and annual performance
+                reviews of my team.
+              </p>
+            </div>
+          </div>
+        </article>
+      </section>
+      <div class="grid grid-cols-2">
+        <section>
+          <h2>education</h2>
+          <h4>Certified scrum master</h4>
+          <p>Scrum Alliance | 2011</p>
+          <h4>Certified usability analyst</h4>
+          <p>Human Factors International | 2009</p>
+          <h4>BSc. Computer Science</h4>
+          <p>University of British Columbia | 2007</p>
+        </section>
+        <section>
+          <h2>interests</h2>
+          <h4>Judo</h4>
+          <p>International competitor<br />European champion | 2020</p>
+          <h4>Usability</h4>
+          <p>Special interest in user experience and accessibility</p>
+          <h4>Cloud architecture</h4>
+          <p>Micro service architecture and observability</p>
+        </section>
+      </div>
+    </main>
+  </div>
+</template>
+
+<style scoped lang="postcss">
+#app {
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+}
+h1 {
+  @apply uppercase text-4xl font-bold;
+  line-height: 3rem;
+}
+h2 {
+  @apply uppercase text-2xl font-semibold;
+  line-height: 2.5;
+}
+h3 {
+  @apply text-xl font-semibold;
+  line-height: 2.5;
+}
+h4 {
+  @apply font-semibold;
+}
+aside.summary {
+  @apply w-40 p-2 border-r-2 border-gray-400;
+}
+ul {
+  @apply list-disc list-inside;
+}
+p {
+  @apply mb-2;
+}
+</style>
+
+<script setup lang="ts">
+import Logo from "./components/Logo.vue";
+</script>
